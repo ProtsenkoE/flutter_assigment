eb82485fcedc9e99db8cc2aaacb7e111d99ab9c1
protsenkoe <protsenkoe@mydigicode.com>
2021-10-22 01:42:19 +0400

    assignment 2_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,72 +11,142 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Assignment 2_2',
-      color: Colors.yellow,
+      title: 'Assignment 2_1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            color: Colors.lightGreen,
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+        body: Center(
+          child: SafeArea(
+            child: SizedBox(
+              height: 200.0,
+              width: 200.0,
+              child: Row(
+                children: [
+                  Column(
+                    children: const [
+                      TopBox(),
+                      InnerBox(),
+                      InnerBox(),
+                      BottomLeftBox(),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        color: Colors.yellow,
-                        height: 60,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        children: const [
+                          ArcBox(),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        color: Colors.yellow,
-                        height: 60,
+                      Row(
+                        children: const [
+                          InnerBox(),
+                          InnerBox(),
+                        ],
                       ),
-                    )
-                  ],
-                )
-              ],
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      TopBox(),
+                      InnerBox(),
+                      InnerBox(),
+                      BottomRightBox(),
+                    ],
+                  ),
+                ],
+              ),
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InnerBox extends StatelessWidget {
+  const InnerBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: Colors.green,
+    );
+  }
+}
+
+class TopBox extends StatelessWidget {
+  const TopBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: const BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30.0),
+          topLeft: Radius.circular(30.0),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomLeftBox extends StatelessWidget {
+  const BottomLeftBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: const BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40.0),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomRightBox extends StatelessWidget {
+  const BottomRightBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: const BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(40.0),
+        ),
+      ),
+    );
+  }
+}
+
+class ArcBox extends StatelessWidget {
+  const ArcBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Transform.rotate(
+        angle: 3.14,
+        child: ClipPath(
+          clipper: ArcClipper(),
+          child: Container(
+            height: 10.0,
+            width: 100.0,
+            color: Colors.green,
           ),
         ),
       ),
